c85186092b1edfbac0692aa157183b0494dd4ba1
Aayush-2219 <154007060+Aayush-2219@users.noreply.github.com>
2025-03-28 18:04:08 +0530

    Update models.py

--- models.py
+++ models.py
@@ -14,5 +14,6 @@ class Order(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     user = db.relationship('User', backref='orders')  # 🔥 This adds the missing user relationship
     milk_type = db.Column(db.String(50), nullable=False)
+    litres = db.Column(db.Float, nullable=False)
     timestamp = db.Column(db.DateTime, server_default=db.func.now())
 
